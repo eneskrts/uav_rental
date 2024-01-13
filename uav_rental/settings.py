30e1e3c9e56c4580eb8235d6d0ce37ac253322e1
enes.karatas <enes.karatas@akinon.com>
2024-01-13 14:26:39 +0300

    settings files seperated for deployment

--- uav_rental/settings.py
+++ uav_rental/settings.py
@@ -9,23 +9,27 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, ".env_local"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-u%#g+u0g0%)yso9a+4b3otqh=ff34sy5apeipx-t7oiv8^vbju"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv("HOST")
+]
 
 
 # Application definition
@@ -74,13 +78,16 @@ TEMPLATES = [
 WSGI_APPLICATION = "uav_rental.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # postgresql
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
---
+++ uav_rental/settings_prod.py
@@ -0,0 +1,6 @@
+from .settings import *
+
+
+# static files and static root settings changed for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
