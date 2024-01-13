79ce31fdd507a8ead952228c1b7cd46202dfd352
enes.karatas <enes.karatas@akinon.com>
2024-01-13 17:55:14 +0300

    auth views created for login and register

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'authentication/login.html'
+    # TODO: index page
+    # success_url = reverse_lazy('index')
+
+
+class RegisterView(CreateView):
+    template_name = 'authentication/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+
